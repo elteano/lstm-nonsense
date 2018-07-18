67d9df643a1ba57b58282ab8f6779b11c02c501b
Thomas Tucker <elteano@gmail.com>
2018-07-18 08:42:03 -0700

    Back to softmax, new temperatured prediction model.
    We now use a separate prediction model which is almost identical, except that
    we do not have any dropout layers and instead have a lambda layer which
    modifies the input to the final softmax layer to implement a temperature
    setting. The generated text is sampled from a distribution defined by the
    output of the softmax layer, and the temperature parameter allows us to make
    this distribution more even or more pronounced towards safer outputs.

--- doublstm.py
+++ doublstm.py
@@ -19,6 +19,9 @@ with open('countmonte.txt', encoding='utf8') as f:
 print('Processing file.')
 chars = sorted(list(set(raw_text)))
 print(chars)
+uchars = len(chars)
+print('Number of unique characters: ' + str(uchars))
+uchars = float(uchars)
 char_to_int = {c : i for i, c in enumerate(chars)}
 int_to_char = {i : c for i, c in enumerate(chars)}
 
@@ -38,9 +41,11 @@ n_patterns = len(dataIn)
 
 print('Number of patterns: {:}'.format(n_patterns))
 
+print('Formatting data...')
 procIn = np.reshape(dataIn, (n_patterns, seq_length, 1))
-procIn = procIn / float(len(char_to_int))
+procIn = procIn / uchars
 procOut = np_utils.to_categorical(dataOut)
+print('Data formatted.')
 
 print('Preparing model.')
 model = Sequential()
@@ -48,7 +53,17 @@ model.add(LSTM(256, input_shape=(procIn.shape[1], procIn.shape[2]), return_seque
 model.add(Dropout(0.2))
 model.add(LSTM(256))
 model.add(Dropout(0.2))
-model.add(Dense(procOut.shape[1], activation='relu'))
+model.add(Dense(procOut.shape[1], activation='softmax'))
+
+# This is our prediction model, which forgoes the dropout layers and instead has
+# a lambda layer to implement a temperature parameter
+temp = 0.3
+pred_model = Sequential()
+pred_model.add(LSTM(256, input_shape=(procIn.shape[1], procIn.shape[2]), return_sequences=True))
+pred_model.add(LSTM(256))
+pred_model.add(Lambda(lambda x : x / temp))
+pred_model.add(Dense(procOut.shape[1], activation='softmax'))
+
 print('Model prepared.')
 
 wfiles = glob.glob('weights-relu-*-*-*.hdf5')
@@ -71,6 +86,7 @@ checkpoint = ModelCheckpoint(filepath, monitor='loss', verbose=1, save_best_only
 callbacks_list=[checkpoint]
 
 def pick_ind(arr, k=3):
+    arr = np.array(list(map(lambda x: max(0,x), arr)))
     arr /= np.sum(arr)
     return np.random.choice(range(len(arr)), p=arr)
 
@@ -78,14 +94,16 @@ def pick_ind(arr, k=3):
 for ep in range(20):
     model.fit(procIn, procOut, epochs=ep, batch_size=128, callbacks=callbacks_list, initial_epoch=(ep-1))
 
+    # Copy the weights we got into our prediction model
+    pred_model.set_weights(model.get_weights())
     start = np.random.randint(0, len(dataIn)-1)
     pattern = dataIn[start]
     print('Seed: {:}'.format(''.join([int_to_char[v] for v in pattern])))
-    print('Generated:')
-    for i in range(1000):
+    print('Generated with temp of {:}:'.format(temp))
+    for i in range(500):
         x = np.reshape(pattern, (1, len(pattern), 1))
-        x = x / float(len(char_to_int))
-        prediction = model.predict(x, verbose=0)
+        x = x / uchars
+        prediction = pred_model.predict(x, verbose=0)
         #ind = np.argmax(prediction)
         ind = pick_ind(prediction[0])
         result = int_to_char[ind]
